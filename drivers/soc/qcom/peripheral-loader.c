6ad620a7139a22f2012e345d01133e9167855e52
plushie-neko <sunflowers.warp@gmail.com>
2026-08-07 12:32:44 +0300

    soc: qcom: peripheral-loader: Hardcode sequential loading for modem

--- drivers/soc/qcom/peripheral-loader.c
+++ drivers/soc/qcom/peripheral-loader.c
@@ -931,6 +931,11 @@ static int pil_parse_devicetree(struct pil_desc *desc)
 	}
 	desc->proxy_unvote_irq = clk_ready;
 
+	if (!strcmp(desc->name, "modem")) {
+		dev_info(desc->dev, "Forcing sequential firmware load for modem\n");
+		desc->sequential_load = true;
+	}
+
 	desc->sequential_load = of_property_read_bool(ofnode,
 						"qcom,sequential-fw-load");
 	return 0;
